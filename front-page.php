a1d12b595ff3820881cc5f81040cb7f9d5bed7b5
Bruno Souza <bsdsouza@gmail.com>
2023-05-12 16:14:41 -0300

    Sessão Missão, Visão e Valores

--- front-page.php
+++ front-page.php
@@ -22,6 +22,37 @@
     </div>
   </section>
 
+  <?php if ( have_rows( 'rpt_mvv', get_option('page_on_front') ) ) : ?>
+    <section class="my-20">
+      <div class="container mx-auto flex items-stretch">
+        <?php
+          $i = 0; while ( have_rows( 'rpt_mvv', get_option('page_on_front') ) ) : the_row(); $i++;
+          $ico = get_sub_field('ico');
+          if ( $i === 1 ) {
+            $bg = 'bg-black-light/20';
+          } elseif ( $i === 2 ) {
+            $bg = 'bg-black-light';
+            $color = 'text-white';
+          } else {
+            $bg = 'bg-orange';
+            $color = 'text-black-light';
+          }
+          
+        ?>
+          <div class="flex items-start justify-between w-1/3 p-5 <?php echo $bg; ?>">
+            <figure class="flex w-20 m-0 mr-8">
+              <img src="<?php echo $ico['url']; ?>" class="w-20">
+            </figure>
+            <div class="flex-1">
+              <h3 class="font-bold text-2xl mb-4 <?php echo $color; ?>"><?php the_sub_field('title'); ?></h3>
+              <p class="<?php echo $color; ?>"><?php the_sub_field('description'); ?></p>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      </div>
+    </section>
+  <?php endif; ?>
+
 <?php
   _partials('_end');
   get_footer();
